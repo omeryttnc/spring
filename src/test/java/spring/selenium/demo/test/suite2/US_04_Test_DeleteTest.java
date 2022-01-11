5181d1d04ab1dd5c8e00f67f5e725718e7816187
omeryttnc <67800186+omeryttnc@users.noreply.github.com>
2022-01-11 10:32:16 +0000

    pom.xml

--- src/test/java/spring/selenium/demo/test/suite2/US_04_Test_DeleteTest.java
+++ src/test/java/spring/selenium/demo/test/suite2/US_04_Test_DeleteTest.java
@@ -46,6 +46,7 @@ public class US_04_Test_DeleteTest {
   //  @EnabledOnOs(OS.LINUX)
     public void TC0301(){
 
+
         response = given().accept(ContentType.JSON).when().get(endPoint);
         jsonPath = response.jsonPath();
         int sizeOfData = jsonPath.getInt("meta.pagination.total");
